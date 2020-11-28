20d2b95bbd95125d7bc09a46190e7d794b4b4793
AlbertMitjans <a.mitjanscoma@gmail.com>
2020-11-28 16:17:27 +0100

    Delete get_dataset.sh and get_weights.sh

--- data/get_dataset.sh
+++
@@ -1,6 +0,0 @@
-git clone git@github.com:AlbertMitjans/corner-detection-dataset.git
-
-mv corner-detection-dataset/train_dataset/ .
-mv corner-detection-dataset/val_dataset/ .
-
-rm -rf corner-detection-dataset/
